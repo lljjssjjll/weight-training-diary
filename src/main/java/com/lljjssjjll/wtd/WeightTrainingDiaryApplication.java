a7ddd55aa2a5cda51e68c92f43f59e309bf2467b
lljjssjjll <lljjssjjll@gmail.com>
2020-07-29 14:59:02 +0900

    초기화 설정

---
+++ src/main/java/com/lljjssjjll/wtd/WeightTrainingDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.lljjssjjll.wtd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightTrainingDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightTrainingDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lljjssjjll/wtd/WeightTrainingDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lljjssjjll.wtd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightTrainingDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
